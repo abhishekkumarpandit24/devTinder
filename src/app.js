42e98cd87d72ea6da4fac4beb35d68eca6f42ec6
abhishekkumarpandit24 <pandit.abhishek122@gmail.com>
2024-10-05 21:42:48 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -2,45 +2,20 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-// Handle auth middleware for all requests having admin
-app.use("/admin", adminAuth)
-
-app.post("/user/login", (req, res) => {
-    res.send("User logged in successfully!");
+app.get('/getUserData', (req, res) => {
+    try {
+    res.send("user Data sent");
+      
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 });
 
-app.get("/user", userAuth, (req, res) => {
-    res.send("User Data Sent");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-    // check if the request is authorised
-    // Now don't need this 
-    // const token = "xyz" || req.body?.token;
-    // const isAdminAuthorized = token === "xyz";
-    // if(isAdminAuthorized) {
-    // res.send("All Data sent");
-    // } else {
-    //     res.status(401).send("Not authorised!")
-    // }
-    res.send("All Data sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-    // check if the request is authorised
-    // const token = "xyz" || req.body?.token;
-    // const isAdminAuthorized = token === "xyz";
-    // if(isAdminAuthorized) {
-    //     res.send("Delete a user");
-
-    // } else {
-    //     res.status(401).send("Not authorised!")
-    // }
-    res.send("Delete a user");
-
-});
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        res.status(500).send("Something went wrong!");
+    }
+})
 
 app.listen(4000, () => {
     console.log("Server is successfully running at port 4000...")
