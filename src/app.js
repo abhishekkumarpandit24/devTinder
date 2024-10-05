b63617f38de25b22b48cef4e80089065ccf1082e
abhishekkumarpandit24 <pandit.abhishek122@gmail.com>
2024-10-05 18:06:05 +0530

    next can be used to pass to another handler

--- src/app.js
+++ src/app.js
@@ -3,27 +3,12 @@ const express = require("express");
 const app = express();
 
 // this will only handle GET call to /user
-app.get("/user", (req, res) => {
-    res.send({ firstName: "Abhishek", lastName: "Pandit" })
+app.get("/user", (req, res, next) => {
+    // res.send("Route handler 1")
+    next();
+}, (req, res) => {
+    res.send("2nd response")
 });
-
-app.post('/user', (req, res) => {
-    // saving data to DB
-    res.send("Data successfully saved to the database");
-});
-
-app.delete('/user', (req, res) => {
-    res.send("Deleted successfully!")
-})
-
-// this will match all thje HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the server");
-})
-
-app.use("/", (req, res) => {
-    res.send("Namaste Abhishek")
-})
 
 app.listen(4000, () => {
     console.log("Server is successfully running at port 4000...")
