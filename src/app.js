de7b01d06ce4c661703249e898c9269dc2312daf
abhishekkumarpandit24 <pandit.abhishek122@gmail.com>
2024-10-05 18:10:01 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const app = express();
 
+// GET /users => it checks all the app.xxx('matching route") functions
+
+app.use("/", (req, res, next) => {
+    next();
+})
+
 // this will only handle GET call to /user
 app.get("/user", (req, res, next) => {
     // res.send("Route handler 1")
     next();
-}, (req, res) => {
-    res.send("2nd response")
+}, (req, res, next) => {
+    // res.send("2nd response")
+    next();
+}, (req, res, next) => {
+    res.send("3rd route handler")
 });
 
 app.listen(4000, () => {
