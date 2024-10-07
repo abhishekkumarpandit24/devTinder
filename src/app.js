32a3e6d2e9ba99e01a1969dfa186d8185125328c
abhishekkumarpandit24 <pandit.abhishek122@gmail.com>
2024-10-07 23:11:58 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ app.post("/signup", async (req, res) => {
 
     } catch (err) {
         console.log(err)
+        res.status(400).send('Something went wrong!')
+
     }
 });
 
@@ -63,10 +65,12 @@ app.patch('/user', async(req, res) => {
     const userId = req.body.userId;
     const data = req.body;
     try {
-        const user = await User.findByIdAndUpdate({ _id: userId }, data);
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+            runValidators: true
+        });
         res.send('User updated Successfully!')
     } catch (err){
-        res.status(400).send('Something went wrong!');
+        res.status(400).send('Something went wrong!'+ err.message);
     }
 })
 
